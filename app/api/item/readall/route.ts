45fd1af708c01d1007963e63ec6d9b1cd7dfe183
perforb <dev.perfumed.garden@gmail.com>
2023-12-30 19:30:12 +0900

    add an endpoint for reading all items

---
+++ app/api/item/readall/route.ts
@@ -0,0 +1,18 @@
+import {NextResponse} from "next/server";
+import connectMongoDB from "@config/database";
+import {ItemModel} from "@api/item/schema";
+
+export async function GET() {
+  try {
+    await connectMongoDB();
+    const items = await ItemModel.find();
+    return NextResponse.json({
+      message: "Read all items.",
+      items: items,
+    });
+  } catch (e) {
+    return NextResponse.json({
+      message: "Failed to read items."
+    });
+  }
+}
